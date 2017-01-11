bc85797676b48dde8877a306480431feb940291f
xxxgitone <xxxgit@sina.com>
2017-01-11 14:34:40 +0800

    登录和登出

--- models/users.js
+++ models/users.js
@@ -4,6 +4,15 @@ module.exports = {
 	//注册一个用户
 	create: function create(user) {
 		return User.create(user).exec();
-	}
+	},
 	
+	//通过用户名获取用户信息
+	getUserByName: function getUserByName(name) {
+		return User.findOne({ name: name})
+					.addCreatedAt()
+					.exec();
+					//addCreatedAt自定义插件（通过 _id 生成时间戳
+	},
+
+
 }
